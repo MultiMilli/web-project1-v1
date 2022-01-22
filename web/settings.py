33333108a3b5bc6dd15e219263c74dddfb6e3f61
MultiMilli <xwins2000m@gamil.com>
2022-01-22 16:10:50 +0200

    Added config files

--- web/settings.py
+++ web/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ovpt+53og87_!5#9!3hb#p2-4pnw*d_f_vgcj5=#0s-auk=otc'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-ovpt+53og87_!5#9!3hb#p2-4pnw*d_f_vgcj5=#0s-auk=otc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
